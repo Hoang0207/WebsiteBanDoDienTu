1e008f05348576427c7128199d1ab8ee47d350b9
vbui9 <vbui9@DESKTOP-A3GUJI3>
2024-11-13 11:55:26 +0700

    viet: Dang cap nhat them vao phan gio hang

--- src/main/java/com/group4/controller/GioHangController.java
+++ src/main/java/com/group4/controller/GioHangController.java
@@ -9,24 +9,33 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.group4.dao.GioHangDAO;
+import com.group4.entity.NguoiDung;
 import com.group4.entity.GioHang;
 import com.group4.service.GioHangService;
+import com.group4.service.NguoiDungService;
 
 @Controller
 public class GioHangController {
+	
+	@Autowired
+	NguoiDungService nguoiDungService;
 	
 	@Autowired
     GioHangService gioHangService;
 
     @GetMapping("/GioHang/{maNd}")
     public String getGioHang(Model model, @PathVariable("maNd") String maNd) {
-        List<GioHang> gioHangs = gioHangService.getGioHangByMaNguoiDung(maNd); 
+        List<GioHang> gioHangs = gioHangService.getGioHangByMaNguoiDung(maNd);
+        List<NguoiDung> nguoiDung = nguoiDungService.findAll(); 
         System.out.println(gioHangs);
+        System.out.println(nguoiDung);
         model.addAttribute("gioHangs", gioHangs);
+        model.addAttribute("nguoiDung", nguoiDung);
         model.addAttribute("content","/layout/giohang");
         return "index";
     }
+    
+    
     
     public String deleteGioHangItem(@PathVariable("maNd") int maNd) {
         gioHangService.deleteGioHang(maNd); // Implement this method in GioHangService
