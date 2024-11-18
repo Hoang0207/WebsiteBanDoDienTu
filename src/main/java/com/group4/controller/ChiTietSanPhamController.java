4c118f2d9a96543e42fe49e109bada13f3ceddeb
vbui9 <vbui9@DESKTOP-A3GUJI3>
2024-11-18 12:46:01 +0700

    viet: cap nhat phan html va controller chi tiet san pham

--- src/main/java/com/group4/controller/ChiTietSanPhamController.java
+++ src/main/java/com/group4/controller/ChiTietSanPhamController.java
@@ -1,11 +1,11 @@
 package com.group4.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.group4.entity.SanPham;
 import com.group4.service.SanPhamService;
@@ -15,18 +15,19 @@ public class ChiTietSanPhamController {
     @Autowired
     private SanPhamService productService;
     
-    @GetMapping("/detail")
+    /*@GetMapping("/detail")
 	public String detail(Model model) {
 		model.addAttribute("content","/pages/detail");
 		return "indexLayout";
-	}
+	}*/
     
-    /*
-    @GetMapping("/{id}")
-    public String getProductDetail(@PathVariable("id") String id, Model model) {
-        SanPham product = productService.findById(id).get();
+
+    @GetMapping({"/detail/{maSanPham}"})
+    public String detail(@PathVariable("maSanPham") String maSanPham, Model model) {
+        SanPham product = productService.findById("maSanPham").orElse(null);
         model.addAttribute("product", product);
-        model.addAttribute("content","/layout/chitietsanpham");
-        return "index";
-    }*/
+        model.addAttribute("content","/pages/detail");
+        return "indexLayout";
+    }
+    
 }
