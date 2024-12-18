07468af4f5d8b66f79f0fd2644666d2e3b9023d8
Hoang0207 <giuse02072003@gmail.com>
2024-12-18 07:27:46 +0700

    Chức năng cập nhật thông tin cá nhân

--- src/main/java/com/group4/controller/ThongTinCaNhan.java
+++ src/main/java/com/group4/controller/ThongTinCaNhan.java
@@ -1,11 +1,14 @@
 package com.group4.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.group4.entity.NguoiDung;
 import com.group4.service.NguoiDungService;
@@ -21,7 +24,7 @@ public class ThongTinCaNhan {
 	SessionUtil session;
 
 	@GetMapping("/userform")
-	public String userform(Model model) {
+	public String userform(Model model) {		
 		model.addAttribute("content", "/pages/thongtinnguoidung");
 		NguoiDung nd = ndService.getInSession();
 		model.addAttribute("nd", nd);
@@ -29,7 +32,7 @@ public class ThongTinCaNhan {
 	}
 
 	@PostMapping("/userform/ChangeInfomation")
-	public String changeInfomation(Model model, @ModelAttribute("nd") NguoiDung nd) {
+	public String changeInfomation(Model model, @ModelAttribute("nd") NguoiDung nd, RedirectAttributes redirectAttributes) {
 		System.out.println(nd);
 		NguoiDung ndSession = ndService.getInSession();
 		ndSession.setHoTen(nd.getHoTen());
@@ -38,7 +41,21 @@ public class ThongTinCaNhan {
 		ndSession.setDiaChi(nd.getDiaChi());
 		ndSession.setGioiTinh(nd.getGioiTinh());
 		ndService.save(ndSession);
+		redirectAttributes.addFlashAttribute("SuccessUpdateInfomationMessage", "Cập nhật thông tin cá nhân thành công");
 		return "redirect:/userform";
 	}
 	
+	@PostMapping("/userform/ChangePassword")
+	public String changePassword(Model model,RedirectAttributes redirectAttributes, @RequestParam("NewPass") String newPass, @RequestParam("Retype") String retype) {
+		NguoiDung ndSession = ndService.getInSession();
+		if(newPass.equalsIgnoreCase(retype)) {
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			ndSession.setMatKhau(encoder.encode(newPass));
+			ndService.save(ndSession);
+			redirectAttributes.addFlashAttribute("SuccessUpdatePasswordMessage", "Cập nhật mật khẩu thành công");
+		}else {
+			redirectAttributes.addFlashAttribute("FailedUpdatePasswordMessage", "Mật khẩu nhập lại không khớp");
+		}
+		return "redirect:/userform";
+	}
 }
